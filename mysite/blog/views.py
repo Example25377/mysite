37c0e67a06d87fbc5fa1493177258eee2b6f9c4a
Example25377 <73432724+Example25377@users.noreply.github.com>
2020-10-30 13:38:48 +0200

    Add post_list2, post_list3

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_list2(request):
+    return render(request, 'blog/post_list2.html', {})
+
+def post_list3(request):
+    return render(request, 'blog/post_list3.html', {})
